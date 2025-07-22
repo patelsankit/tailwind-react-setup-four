a94b3c9a917e7c50ac4ca2453bceb782726ed34f
patelsankit <patelsankit16@gmail.com>
2025-07-22 12:22:10 +0530

    Add HomeContent component and update App layout with new styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import HomeContent from './components/HomeContent';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300 dark:text-white text-black">
       <Navbar />
-      {/* Other content can go here */}
+      <HomeContent/>
     </div>
   );
 }
---
+++ src/components/HomeContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeContent = () => {
+  return (
+    <div className='flex flex-col items-center justify-center pt-10'>
+        <h1 className='text-4xl font-bold text-primary'>HomeContent</h1>
+        <h1 className='text-4xl font-bold text-red-800'>HomeContent</h1>
+        <h1 className='text-4xl font-bold text-primary-cst'>HomeContent</h1>
+        <h1 className='text-4xl font-bold text-custom-color'>HomeContent</h1>
+    </div>
+  )
+}
+
+export default HomeContent
